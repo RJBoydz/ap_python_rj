72996894a286684f477c03563a523a7cb13b9f6e
RJBoydz <130468015+RJBoydz@users.noreply.github.com>
2023-09-21 15:08:13 +0000

    first commit

---
+++ Work20.py/work20.py
@@ -0,0 +1,25 @@
+# convert the fp;;owing datatypes into floats.
+a= 12
+b= 100
+print(float(a))
+print(float(b))
+# convert the following floats into integers
+c= 12.34
+d= 23.33
+print(int(c))
+print(int(d))
+# modify the code below to print an teger number.
+num_one = 20
+num_two = 23.45
+
+print(int(num_one + num_two))
+
+#print(num_one+num_two)
+
+# modifty the following code to
+name_of_pet = "Spuny"
+age_of_pet = 10
+
+print(str("this is my dog"+ name_of_pet+".He is"+age_of_pet"years old."))
+
+#insructor note
