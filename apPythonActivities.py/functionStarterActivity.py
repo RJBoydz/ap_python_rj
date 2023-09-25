aa3991df2453c4db29ac17e25f9c13d767b134ed
RJBoydz <130468015+RJBoydz@users.noreply.github.com>
2023-09-25 14:00:11 +0000

    Weekend_homework

---
+++ apPythonActivities.py/functionStarterActivity.py
@@ -0,0 +1,40 @@
+#def welcomeToClass():
+#    print('welcome to class')
+#print('----------------------------')
+
+#def custom_Welcome_Message(name):
+#    print('hello ' + name +' welcome to Python class')
+
+#custom_Welcome_Message(str(1293202029))
+
+#def calculate(numberA, numberB):
+#    print(numberA + numberB)
+
+#calculate(10,10)
+
+# 1. In your own words, describe what a function is
+∞ # a relation between a set of inputs having one output each.
+
+# 2. What is are function parameters and arguments and describe
+# the difference between the 2.
+∞ #A parameter is a variable in a function definition. It is a placeholder and hence does not
+∞  #have a concrete value. An argument is a value passed during function invocation. 
+
+# 3. write a function that will print out a welcome message
+# that includes a users name. You will need to use parameters and arguments
+name = input('What is your name')
+print('Hi Im RJ Boyd ', name , 'how is your day')
+
+# 4. Write a function that will do a calculation that takes 3 parameters.
+# Your function can do any of the arithmatic operators (add, subtract, multiply, divide)
+K = 33
+L = 340
+P = 777
+print(K+L+P)
+
+# 5. Write a function that will output a message to a user, telling them
+# what class they have next after this one. this code should use a 
+# variable to pass a value into the parameter of a function. The variable should
+# be real, user data- not hard coded data.
+X= 'Your next class will be coding'
+print(X)
