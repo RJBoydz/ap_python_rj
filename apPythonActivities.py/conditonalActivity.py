e5fb1e7e878018bff55140786a7689878346d110
RJBoydz <130468015+RJBoydz@users.noreply.github.com>
2023-09-28 15:37:26 +0000

    classwork_9/28/23

--- apPythonActivities.py/conditonalActivity.py
+++ apPythonActivities.py/conditonalActivity.py
@@ -5,14 +5,17 @@
 
 # 2. In your own words, describe what 
 # a conditional statement (if/else) is 
-'The if/else statement reads a block of code. if a specified part of the condition is true.'
-
+'The if-else statement is used to execute both the true and false part of a given '
+'function If the function is true, the if block code is executed and if the function '
+' is false, the else block code is executed.'
 
 # 3. write a simple conditional state using a comparision operator(greater than, less than, etc. )
 a = 666
 b = 99999
 if b > a:
   print("b is greater than a")
+else:
+   print('b is not greater than a')
 
 # 3. Write a conditional statement for food in the refridgerator.
 # your conditional statement should be wrapped in a function that takes one (1)
@@ -23,20 +26,16 @@ if b > a:
 # When you call your function there should be an argument that accepts 
 # a boolean. 
 
-frid = True
+# boolean as an argument in our fiction (value will either true or false)
 
-if frid == True:
-   print("TIme to cook")
-else:
-   print('Is there food in the refridgerator ')
+def fridstuff(food):
+   if food == True:
+      print("TIme to cook")
+   else:
+         print('No food in the fridge')
 
-weekday= False
+fridstuff(True)
 
-
-if weekday == False:
-   print("Not ant food in the fridge, go out and buy some")
-else:
-   print("no food")
 
 # 4. Create a function that checks the inventory of cereal for a store. 
 # your function should parameter should accept an integer. In your function
@@ -44,22 +43,11 @@ else:
 # If there is more than 10 boxes, print "inventory full", else if there are less than 
 # 10 boxes print "we need to order more cereal".
 
-
-inventory= True
-
-
-if inventory == True:
-   print("DO you need anymore cereal")
+def inventory_of_cerel(amount):
+   if amount > 10:
+      print ('We have enogught cereal in stack')
 else:
-   print("no we have enough")
+print('We do need more cereal')
 
-def buyOFX(amountOfCereal):
-  EnoughCereal = 10
-  if amountOfCereal >= EnoughCereal:
-     print("We have enough boxes")
-  else:
-    print('We will be good for now')
-
-buyOFX(10)
-
-
+print('______________________')
+inventory_of_cerel(8)  
