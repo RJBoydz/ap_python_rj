fdb869ceeaecda35498584d78e221401c1b8194b
RJBoydz <130468015+RJBoydz@users.noreply.github.com>
2023-10-16 15:02:15 +0000

    Async work Oct16.py

---
+++ apPythonActivities.py/Async work Oct16.py
@@ -0,0 +1,18 @@
+#My Python software program will be based off of a program called Spotify. It is a digital music, podcast,and video service that gives you access to millions of songs and other content from creators all over the world.  
+#Spotify only gives access to music and podcasts through our apps. Our licensing means there's no way to stream or show content outside of the app.
+
+#Their basic functions are to play music for free, but you can also choose to upgrade to Spotify Premium which is $10.99 per month. This allows access to all the Premium features such as no ad interruptions, on demand playback, and downloading tracks for offline listening. Spotify was founded by Daniel Ek and Martin Lorentzon as a way to deal with the problem of music piracy. Before the music streaming services got popular, there were many who downloaded pirated music files. 
+
+#This was a growing challenge for the whole music industry and is the base of Spotify history. Spotify is one of the largest music streaming service platforms with more than 433 million monthly active users. Python is primarily used at Spotify to serve Backend services and Data analysis. 
+
+#The backend of Spotify is made up of numerous interdependent services that are connected via a custom messaging protocol using ZeroMQ. As per experts, 80% of these services are written in Python programming language.Python is a good fit for Spotify as it puts a lot of emphasis on speed. Spotify also heavily rely on Python async libraries to aid IO-bound services.
+
+#Spotify may be great for music and songs, but there are some down sides, some being a low sound quality, so if you are trying to play a song, the sound may come in poorly, limits to downloading, Spotfiy wouldn’t let you download all the songs you want there will be a limit, and spotify uses a lot of data  to streaming music, the amount is  between 10MB and 144MB per hour, and after that it starts to get expensive.
+
+#In conclusion, Spotify is a great app for playing music, and python had a big impact on its development and how people now and freely listen to their songs without pirating the music file, allowing it to function in its proper way to suit the needs of its consumers.
+
+#the websites i used were 
+#https://tinker.ly/top-websites-built-using-python/#:~:text=Python%20is%20primarily%20used%20at,written%20in%20Python%20programming%20language.
+#https://startuptalky.com/spotify_success_story/#:~:text=Spotify%20was%20founded%20by%20Daniel,the%20base%20of%20Spotify%20history.
+#https://support.spotify.com/us/article/what-is-spotify/
+#https://freeyourmusic.com/blog/spotify-cons
