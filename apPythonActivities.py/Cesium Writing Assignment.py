aeecb3cc173a77572dd0d2c52980dbe212d4ea88
RJBoydz <130468015+RJBoydz@users.noreply.github.com>
2023-11-01 00:11:40 +0000

    Cesium Writing Assignment

---
+++ apPythonActivities.py/Cesium Writing Assignment.py
@@ -0,0 +1,14 @@
+# One of the major project is th scan landscapes with of there drones to make a digital version of said
+# landscape with a website or newly introduced VR headset. This allows the user to see and plan out how to 
+# work or start construction on a piece of land.
+
+# The XR software could be use to predict how much work needs to be done on a piece of land. Gaming engine
+# was created for game development or other complicated programs.
+
+# They take massive amounts of data and shrink it down to mangeable levels for a computer program to use.
+# They have a 3d digital image of the world and this site can nearly go anywhere in the world. The data
+# will decide where in the world you can go.
+
+# This data map can only be made through real world means, anything fictional can not be made. The companies
+# primary goal was to create 3d geospatial visualization with that being finish, there new goal is to upgrade
+# and improve upon the 3d program to make sure people use it.
